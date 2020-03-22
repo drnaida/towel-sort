753e1073c5d637dfdd3641da88f8d00d499d62ff
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-03-22 14:52:52 +0300

    feat: add towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  //Protection from empty array and array with one element
+  if (!matrix || matrix.length == 0) {
+    return [];
+  }
+  if (matrix.length == 1) {
+    return matrix;
+  }
+  //Important values
+  let isReversed = false;
+  let result = [];
+
+  matrix.forEach(item => {
+    if (isReversed) {
+      item.reverse().forEach(i => {
+        result.push(i);
+      });
+      isReversed = false;
+    } else {
+      item.forEach(i => {
+        result.push(i);
+      });
+      isReversed = true;
+    }
+  });
+  return result;
 }
